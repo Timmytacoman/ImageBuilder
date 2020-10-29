42222afb767e1fe03c91e70bc3ada8e9d228e67b
Timmytacoman <Timothyb4man@gmail.com>
2020-10-29 16:21:33 -0600

    adding some challenges

---
+++ challenges/list_of_multiples.py
@@ -0,0 +1,5 @@
+def list_of_multiples(num, length):
+    my_list = []
+    for i in range(1, length + 1):
+        my_list.append(num * i)
+    return my_list
---
+++ challenges/simple_oop_calculator.py
@@ -0,0 +1,7 @@
+class Calculator:
+    # Write methods to add(), subtract(), multiply() and divide()
+    def add(self, n1, n2):
+        pass
+
+    def subtract(self, n1, n2):
+        pass
