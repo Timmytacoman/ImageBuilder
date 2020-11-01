6bcc38db579b90dd2506a2360b53f3712ee4e807
Timmytacoman <72770854+Timmytacoman@users.noreply.github.com>
2020-11-01 12:40:15 -0700

    Created a better directory system for finding blocks

--- img_to_blocks/main.py
+++ img_to_blocks/main.py
@@ -1,14 +1,5 @@
 from PIL import Image
 
-# open image to process
-im = Image.open(
-    r'C:\Users\Timothy\PycharmProjects\misc_projects\img_to_blocks\images\unnamed (1).jpg')  # Can be many different formats.
-
-# get width and height
-width, height = im.size
-# load all pixel rgbs
-pix = im.load()
-
 
 def get_distance(v1, v2):
     """math bruh"""
@@ -17,21 +8,41 @@ def get_distance(v1, v2):
     return ((ex - x0) ** 2 + (ey - y0) ** 2 + (ez - z0) ** 2) ** .5
 
 
-def get_block_equivalence(values):
+def get_directory(path_to_file):
+    file = open(path_to_file, "r")
+    line_counter = 0
+    dictionary = {}
+    for line in file:
+        if line_counter > 0:
+            # search from back to front for ','
+            # split into two parts
+            comma_is_present = False
+            counter = -1
+            while not comma_is_present:
+                char = line[counter]
+                if char == ",":
+                    break
+                else:
+                    counter -= 1
+
+            # find rgb
+            rgb = line[:counter - 1]
+            placeholder = rgb.find("rgb")
+            rgb = rgb[placeholder + 4:-1]
+            tuple_of_rgb = tuple(map(int, rgb.split(',')))
+
+            # find block and chop off new line at end
+            block = line[counter + 1:-1]
+
+            dictionary[tuple_of_rgb] = block
+
+        else:
+            line_counter = 1
+    return dictionary
+
+
+def get_block_equivalence(values, dictionary):
     """Find the corresponding block from a dictionary setup"""
-
-
-    # Improvements coming
-    dictionary = {
-        (255, 0, 0): "REDSTONE_BLOCK",
-        (0, 255, 0): "GRASS",
-        (62, 187, 99): "GRASS",
-        (0, 0, 255): "LAPIS_BLOCK",
-        (255, 255, 255): "IRON_BLOCK",
-        (0, 255, 255): "DIAMOND_BLOCK",
-        (153, 204, 255): "MELON_BLOCK",
-        (128, 0, 0): "NETHERRACK",
-    }
 
     smallest = 1e5
     for i in dictionary:
@@ -42,16 +53,37 @@ def get_block_equivalence(values):
     return dictionary[key]
 
 
-# scan each pixel in image
-s = ""
-for y in range(height):
-    for x in range(width):
-        s += get_block_equivalence(pix[x, y]) + ", "
+def scan():
+    # open image to process
+    im = Image.open(
+        r'C:\Users\Timothy\PycharmProjects\misc_projects\img_to_blocks\images\rainbow.jpg')  # Can be many different formats.
 
-# chop off last ", "
-s = s[:-2]
+    # get width and height
+    width, height = im.size
+    # load all pixel rgbs
+    pix = im.load()
 
-# write to file
-file = open("block_data.txt", "w")
-file.write(s)
-file.close()
+    # load rgb_block_data.csv directory
+    directory = get_directory(r"C:\Users\Timothy\PycharmProjects\misc_projects\img_to_blocks\rgb_block_data.csv")
+
+    # scan each pixel in image
+    s = str(width) + ", " + str(height) + ", "
+    for y in range(height):
+        for x in range(width):
+            s += get_block_equivalence(pix[x, y], directory) + ", "
+
+    # chop off last ", "
+    s = s[:-2]
+    return s
+
+
+def write_block_data(s):
+    # write to file
+    file = open("block_data.txt", "w")
+    file.write(s)
+    file.close()
+
+
+if __name__ == "__main__":
+    """Warning: must add new line at end of rgb_block_data.csv!!!"""
+    write_block_data(scan())
