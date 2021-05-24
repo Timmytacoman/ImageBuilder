ec0ffa647d26fba9a93bde4df8e1430cd151d6f0
Timmytacoman <72770854+Timmytacoman@users.noreply.github.com>
2021-05-23 20:58:03 -0600

    fixed appearance

--- dice_roll/high_rollah.py
+++ dice_roll/high_rollah.py
@@ -4,8 +4,12 @@ numSides = 20
 numRolls = 1_000_000
 numWins = 0
 
+print("-------------------")
+print("Starting rolls")
+print("-------------------")
+
 for i in range(numRolls):
     if random.randint(1, numSides) > random.randint(1, numSides):
         numWins += 1
-    print(f"{numWins / numRolls * 100}%")
 
+print(f"{numWins / numRolls * 100}% win rate")
